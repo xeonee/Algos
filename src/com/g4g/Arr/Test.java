78ea47c4eac3f88509db867d4a83be0e80073f57
Amardeep Chaubey <amar.xeon@gmail.com>
2015-12-03 23:20:54 +0530

    reverse Array
    reverse Array

--- src/com/g4g/Arr/Test.java
+++ src/com/g4g/Arr/Test.java
@@ -2,6 +2,21 @@ package com.g4g.Arr;
 
 public class Test {
 	public static void main(String[] args) {
-		
+		int a[] = {1, 2, 3, 4, 5, 6};
+		reverse(a, 0, a.length-1);
+		for(int n : a)
+			System.out.print(n+" ");
+	}
+
+	private static void reverse(int[] a, int start, int end) {
+		int temp = 0;
+		if(start>= end)
+			return;
+
+		temp = a[start];
+		a[start] = a[end];
+		a[end] = temp;
+		reverse(a, start+1, end-1);
+
 	}
 }
